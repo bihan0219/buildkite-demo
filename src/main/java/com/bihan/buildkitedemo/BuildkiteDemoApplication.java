a3656abbff929d88ab8192b4caca0246d6f34e59
NatasG <gaoshuai1205@gmail.com>
2018-09-12 23:06:11 +0800

    add springboot

---
+++ src/main/java/com/bihan/buildkitedemo/BuildkiteDemoApplication.java
@@ -0,0 +1,12 @@
+package com.bihan.buildkitedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildkiteDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildkiteDemoApplication.class, args);
+	}
+}
